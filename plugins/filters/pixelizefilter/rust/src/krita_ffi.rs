0c3ef680cc1d6c7c86bc9ea2a6670b8434a0e2a8
Alvin Wong <alvinhochun@gmail.com>
2019-03-16 20:01:37 +0800

    Move KisSequentialIteratorBase and friends to a mod

--- plugins/filters/pixelizefilter/rust/src/krita_ffi.rs
+++ plugins/filters/pixelizefilter/rust/src/krita_ffi.rs
@@ -1,3 +1,9 @@
+mod kis_sequential_iterators;
+
+pub use kis_sequential_iterators::{
+    KisSequentialConstIterator, KisSequentialIterator, KisSequentialIteratorBase,
+};
+
 extern "C" {
     fn kisSequentialConstIteratorNextPixelCallback(it: *mut KisSequentialConstIterator) -> bool;
     fn kisSequentialConstIteratorOldRawDataCallback(
@@ -11,46 +17,6 @@ extern "C" {
         n_colors: u32,
         dst: *mut KoColorData,
     );
-}
-
-pub trait KisSequentialIteratorBase {
-    fn next_pixel(&mut self) -> bool;
-}
-
-#[repr(C)]
-pub struct KisSequentialConstIterator {
-    _private: [u8; 0],
-}
-
-impl KisSequentialConstIterator {
-    pub unsafe fn old_raw_data(&self, pixel_size: u32) -> &'_ [u8] {
-        let old_raw_data_ptr = kisSequentialConstIteratorOldRawDataCallback(self);
-        std::slice::from_raw_parts(old_raw_data_ptr, pixel_size as usize)
-    }
-}
-
-impl KisSequentialIteratorBase for KisSequentialConstIterator {
-    fn next_pixel(&mut self) -> bool {
-        unsafe { kisSequentialConstIteratorNextPixelCallback(self) }
-    }
-}
-
-#[repr(C)]
-pub struct KisSequentialIterator {
-    _private: [u8; 0],
-}
-
-impl KisSequentialIterator {
-    pub unsafe fn raw_data_mut(&self, pixel_size: u32) -> &'_ mut [u8] {
-        let raw_data_ptr = kisSequentialIteratorRawDataCallback(self);
-        std::slice::from_raw_parts_mut(raw_data_ptr, pixel_size as usize)
-    }
-}
-
-impl KisSequentialIteratorBase for KisSequentialIterator {
-    fn next_pixel(&mut self) -> bool {
-        unsafe { kisSequentialIteratorNextPixelCallback(self) }
-    }
 }
 
 #[repr(C)]
